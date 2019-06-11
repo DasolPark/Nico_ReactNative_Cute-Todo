f7f18daa0b03b539f3b4d850201a62ee819d7c27
Dasol Park <dasol.park615@gmail.com>
2019-06-12 02:24:45 +0800

    5. Coding the UI part Three - To Do Component

--- App.js
+++ App.js
@@ -32,7 +32,7 @@ class App extends React.Component {
             returnKeyType={"done"}
             autoCorrect={false}
           />
-          <ScrollView>
+          <ScrollView contentContainerStyle={styles.toDos}>
             <ToDo />
           </ScrollView>
         </View>
@@ -85,6 +85,9 @@ const styles = StyleSheet.create({
     borderBottomColor: "#bbb",
     borderBottomWidth: 1,
     fontSize: 25
+  },
+  toDos: {
+    alignItems: "center"
   }
 });
 
